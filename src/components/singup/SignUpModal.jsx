312260208cd18d908927ed5051dc349247e7908e
Victor <vitya.ryabkov@gmail.com>
2020-03-04 20:04:34 +0300

    Feature: change sign up structure

--- src/components/singup/SignUpModal.jsx
+++ src/components/singup/SignUpModal.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
-import GoogleLogin from 'react-google-login';
+import React, {
+  useState
+} from 'react';
 
 // Lang
 import {
   MultiLanguage
 } from '../../helpers/Multilang';
 
+// Components 
+import UnAuthorized from './unAuthorized';
+import Authorized from './authorized';
+
 
 const SignUpModal = props => {
 
   const {
     SignUpModalRef,
-    CloseModalFun
+    CloseModalFun,
+    isSignIn,
+    setIsSignIn
   } = props;
+
+  const [
+    profileData,
+    setProfileData
+  ] = useState({});
+
+
+  const GoogleSignIn = responce => {
+    setProfileData(responce.profileObj)
+    setIsSignIn(true);
+    console.log(responce.profileObj)
+  };
+
+  const GoogleLogOut = () => {
+    setIsSignIn(false);
+    setProfileData({});
+  }
 
 
 
@@ -22,24 +46,16 @@ const SignUpModal = props => {
          onClick = { CloseModalFun }    >
       <div className="signup-wrap default-innerModal">
         <h1>
-        {MultiLanguage.rus.buttons.signUp}
+          {MultiLanguage.rus.buttons.signUp}
         </h1>
-        <div className="signup">
-          <GoogleLogin  
-            clientId="440508018667-u45huetcekpp65kgjh4kpmeb5e16j1bk.apps.googleusercontent.com"
-            className="SingInGoogle-button"
-            buttonText="Sign in with Google"
-            onSuccess={ (responce) => {console.log(responce)} }
-            onFailure={ (err) => {console.log(err)} }
-            cookiePolicy={'single_host_origin'}    />
+        
+      {
+        isSignIn ? 
+          <Authorized profileData = {profileData}
+                      GoogleLogOut = {GoogleLogOut}   /> :
+          <UnAuthorized GoogleSignIn = {GoogleSignIn}/>
+      }
 
-
-          <div id="appleid-signin" 
-               className="signin-button" 
-               data-color="black" 
-               data-border="false" 
-               data-type="sign in"></div>
-        </div>
       </div>
     </div>
   )

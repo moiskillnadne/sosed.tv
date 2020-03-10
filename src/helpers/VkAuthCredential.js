6df8a218bacf2a47c346c0f192bb372198083fd8
Victor <vitya.ryabkov@gmail.com>
2020-03-10 15:57:48 +0300

    Feature: vk auth credential

---
+++ src/helpers/VkAuthCredential.js
@@ -0,0 +1,10 @@
+export const VkAuth = {
+  link: 'https://oauth.vk.com/authorize',
+  client_id: '7346579',
+  client_secret: 'YZ0RJQ4VTnL17fhieRgQ',
+  display: 'page',
+  redirect_uri: 'http://sosed.tv',
+  scope: 'friends',
+  response_type: 'code',
+  v: '5.103',
+};

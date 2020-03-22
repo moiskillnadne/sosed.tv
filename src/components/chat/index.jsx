e935090c0dcb78881bd6b7adac4be96dd4b3b6d6
Victor <vitya.ryabkov@gmail.com>
2020-03-22 09:25:55 +0300

    Feature: create chat maket

--- src/components/chat/index.jsx
+++ src/components/chat/index.jsx
@@ -1,9 +1,82 @@
-import React from 'react';
+import React, {
+  useState,
+  useRef
+} from 'react';
 
-const ChatContainer = () => {
+// Components
+import Chat from './Chat';
+import Promotion from './Promotion';
+import GoogleLogin from 'react-google-login';
+
+const ChatContainer = props => {
+
+  const GButtonRef = useRef(null);
+  const GreetingsRef = useRef(null);
+
+  const {
+    isSignIn,
+    setIsSignIn
+  } = props;
+
+  const [
+    profileData,
+    setProfileData
+   ] = useState({});
+
+  const GoogleSignIn = responce => {
+    setProfileData(responce.profileObj);
+
+    GButtonRef.current.style.display = 'none';
+    GreetingsRef.current.style.display = 'block';
+    setTimeout(() => {
+      GreetingsRef.current.style.opacity = '0';
+    }, 1000)
+
+    setTimeout(() => {
+      setIsSignIn(true);
+    }, 2000)
+    console.log(responce.profileObj);
+  };
+
   return (
     <div className="chat-container">
       
+      <Promotion />
+
+      <div className="livechat">
+      {
+        isSignIn ? 
+          <Chat profileData = { profileData }/> : 
+          <div className="blur-block">
+
+            <Chat />
+
+          </div>
+      }
+
+          <div className='signin-Google'
+               ref={GButtonRef} >
+            <GoogleLogin  
+                clientId="440508018667-u45huetcekpp65kgjh4kpmeb5e16j1bk.apps.googleusercontent.com"
+                className="SingInGoogle-button"
+                buttonText="Sign in with Google"
+                onSuccess={ GoogleSignIn }
+                onFailure={ (err) => {console.log(err)} }
+                cookiePolicy={'single_host_origin'}    />
+          </div>
+          <div className='greetings-chat'
+               ref={GreetingsRef}>
+            <h2>
+              Добро пожаловать в чат, <br/>
+              <span>
+                {profileData.name}
+              </span>
+            </h2>
+          </div>
+
+      </div>
+
+
     </div>
   );
 };
